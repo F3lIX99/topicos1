3424c6a7247488a72f3d868ff9353c8211d3f8e5
F3lIX99 <felixtorresmuena@gmail.com>
2022-09-11 22:50:40 -0400

    agregamos algunas weaitas

--- hyperLogLog.h
+++ hyperLogLog.h
@@ -2,18 +2,24 @@
 #include <string>
 #include <math.h>
 
+using namespace std;
+
 class hyperLogLog{
     private:
         int m;
         hash<string>h2;
         float alfa16,alfa32,alfa64,alfam;
-        double E,M[m];
-        int m;
+        double E;
+        double* registros;
+        unsigned long long x;
+        int j;
         
+        //unsigned int s1[],s2[];
     public:
         hyperLogLog(int b);
         double estimacion();
         double correccion();
-        void merge(long long* s1, long long*s2);
+        void update(string linea);
+        void merge(unsigned int s1[], unsigned int s2[]);
         void jaccard();
 };
